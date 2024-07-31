f9bbf892ece738d2c4690fd29efaaedef93ba0d9
elisa-sol <elizaveta-2002@bk.ru>
2024-08-01 01:31:26 +0300

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,23 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Порт, на котором будет запускаться ваше приложение
+    open: true, // Открывать приложение в браузере автоматически при запуске
+  },
+  build: {
+    outDir: 'build', // Папка, в которую будет собираться продакшн-версия
+  },
+  resolve: {
+    alias: {
+      '@': '/src', // Создает псевдоним '@' для директории 'src'
+    },
+  },
+  css: {
+    modules: {
+      // Конфигурация CSS-модулей
+      scopeBehaviour: 'local', // 'local' или 'global'
+      generateScopedName: '[name]__[local]___[hash:base64:5]', // Формат для имен CSS-классов
+    },
+  },
 });
